a29479cfa59315e3a11cbd2f3e3f901e3d4c52bf
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-05-03 21:43:38 +0200

    add method to author model and associate it with a field. add field with block to resolve object data.

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,9 +1,19 @@
 Types::AuthorType = GraphQL::ObjectType.define do
   name 'AuthorType'
 
-  field :first_name, !types.String
+  field :first_name, !types.String do
+    description 'authors first name'
+  end
   field :last_name, types.String
   field :birth_year, types.Int
   field :is_alive, types.Boolean
   field :id, types.ID
+
+  field :full_name, types.String
+  field :personel_data, types.String do
+    description 'show data of an author'
+    resolve ->(obj, _args, _ctx) do
+      "#{obj.first_name} #{obj.last_name}, born #{obj.birth_year}"
+    end
+  end
 end
