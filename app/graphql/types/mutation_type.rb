e59d6d4188712a1962c679a92a5cb42aadcf3aeb
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-05-08 20:09:27 +0200

    refactor create author with a function

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,14 +9,5 @@ Types::MutationType = GraphQL::ObjectType.define do
     }
   end
 
-  field :createAuthor, Types::AuthorType do
-    argument :first_name, types.String
-    argument :last_name, types.String
-    argument :birth_year, types.Int
-    argument :is_alive, types.Boolean
-
-    resolve ->(_obj, args, _ctx) do
-      Author.create args.to_h
-    end
-  end
+  field :createAuthor, function: Mutations::CreateAuthor.new
 end
