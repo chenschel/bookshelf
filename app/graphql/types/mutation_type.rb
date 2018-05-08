a94b83b85b4ab731767dd75b13cb095eacf0c17f
Christopher Henschel <christopher.henschel@sumcumo.com>
2018-05-08 19:56:26 +0200

    create author with a mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,4 +8,15 @@ Types::MutationType = GraphQL::ObjectType.define do
       'Hello World!'
     }
   end
+
+  field :createAuthor, Types::AuthorType do
+    argument :first_name, types.String
+    argument :last_name, types.String
+    argument :birth_year, types.Int
+    argument :is_alive, types.Boolean
+
+    resolve ->(_obj, args, _ctx) do
+      Author.create args.to_h
+    end
+  end
 end
